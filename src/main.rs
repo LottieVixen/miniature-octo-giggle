9ba78ff7db908941bdaaadb5179b483b63120f80
Lilith645 <lilith@inet-sys.com>
2017-01-17 17:58:23 +1000

    Added basic Fragment and vertex shaders, now draws red triangle, fixed multiple warnings organised draw paramenter passing properly Changes committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,67 @@
 #[macro_use]
 
 extern crate glium;
+
 use glium::Surface;
+use glium::DisplayBuild;
+
+#[derive(Copy, Clone)] // Some random thing to define the struct to do stuff? i think?
+struct Vertex {
+  position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn triangle() -> Vec<Vertex> {
+  vec![Vertex { position: [-0.5, -0.5] }, Vertex { position: [ 0.0,  0.5] }, Vertex { position: [ 0.5, -0.25] }]
+}
 
 // That useful drawing function
-fn draw(mut screen: glium::Frame) {
-  // Set screen background colour
-  screen.clear_color(0.0, 0.0, 1.0, 1.0);
+fn draw(display: &glium::backend::glutin_backend::GlutinFacade, shaders: &glium::Program) {
+  let mut render = display.draw();
+  
+  // Set render background colour
+  render.clear_color(0.0, 0.0, 1.0, 1.0);
+  
+  let vertex_buffer = glium::VertexBuffer::new(display, &triangle()).unwrap();
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+  
+  render.draw(&vertex_buffer, &indices, &shaders, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
   
   // Draw screeb
-  screen.finish().unwrap();
+  render.finish().unwrap();
 }
 
 fn main() {
   println!("Hello, world!");
   
-  use glium::DisplayBuild;
+  // Them Shaders
+  let vertex_shader = r#"
+    #version 140
+    
+    in vec2 position;
+    
+    void main() {
+      gl_Position = vec4(position, 0.0, 1.0);
+    }
+  "#;
+
+  let fragment_shader = r#"
+    #version 140
+    
+    out vec4 color;
+    
+    void main() {
+      color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+  "#;
   
   let display = glium::glutin::WindowBuilder::new().with_dimensions(800, 600).
                                                     with_title(format!("TrucksTrucksandMoreTrucks")).
                                                     build_glium().unwrap();
-
-  loop {
-    let mut screen = display.draw();
+  let shaders = glium::Program::from_source(&display, vertex_shader, fragment_shader, None).unwrap();
   
+  loop {
     // Them helpful poll things ya know
     for ev in display.poll_events() {
       match ev {
@@ -31,7 +69,7 @@ fn main() {
       }
     }
     
-    // Draw them things on the screen
-    draw(screen);
+    // Draw them things on the render
+    draw(&display, &shaders);
   }
 }
